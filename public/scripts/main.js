dff1a6ac739624eb8711a5746b4c38955a471e1d
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-24 20:54:53 -0300

    rota do formulario completa

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,13 +16,12 @@ function handleClick(event, check = true){
     
     const text = (check) ? "Marcar como lida" : "Excluir";
     const slug = (check) ? "check" : "delete";
-      
-    
+          
     const roomId = document.querySelector("#room-id").dataset.id;
-
+    const questionId = event.target.dataset.id;
 
     const form = document.querySelector(".modal form");
-    form.setAttribute("action", `/room/${roomId}/:question/${slug}`);
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
 
     modalTitle.innerHTML = `${text} esta pergunta?`;
     modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
