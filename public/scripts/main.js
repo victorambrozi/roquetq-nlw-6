2da277a673a92ca2cd828057c24a562f91df45fb
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-23 22:23:39 -0300

    aplica logica no modal

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+import Modal from './modal.js';
+
+
+const modal = Modal();
+
+const checkButtons = document.querySelectorAll(".actions a.check");
+
+
+checkButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        modal.open();
+    });
+})
+
+const deleteButton = document.querySelectorAll(".actions a.delete");
+
+deleteButton.forEach((button) => {
+    button.addEventListener("click", () => {
+        modal.open();
+    });
+})
