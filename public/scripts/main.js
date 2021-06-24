c574d70f99b5eb8e5f2219acfc027e90a3d402c6
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-23 23:10:28 -0300

    aplica logica nos botoes do modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,19 +3,31 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
+
+
 const checkButtons = document.querySelectorAll(".actions a.check");
 
+function handleClick(event, check = true){
+    const text = (check) ? "Marcar como lida" : "Excluir";
+
+    modalTitle.innerHTML = `${text} esta pergunta?`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+
+
+    modal.open();
+} 
 
 checkButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        modal.open();
-    });
+    button.addEventListener("click", handleClick);
 })
 
 const deleteButton = document.querySelectorAll(".actions a.delete");
 
 deleteButton.forEach((button) => {
-    button.addEventListener("click", () => {
-        modal.open();
-    });
+    button.addEventListener("click", (event) => handleClick(event, false));
 })
+
