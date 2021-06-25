e50170c4161971fa2ead6856ec3ce3a815584ff9
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-24 23:50:34 -0300

    cria salas aleatorias

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,7 +1,13 @@
 module.exports = {
     create(req, res) {
-        let roomId = 123456;
-        
+        let roomId;
+
+        for (let i = 0; i < 6; i++) {
+
+            (i == 0) ? 
+                roomId = Math.floor(Math.random() * 10).toString() : 
+                roomId += Math.floor(Math.random() * 10).toString();
+        }
         res.redirect(`/room/${roomId}`);
     }
 }
