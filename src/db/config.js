454b0087269d5ef5764f84043078e907f793e68b
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-24 23:38:24 -0300

    cria as tabelas do banco de dados e configura a inicializaçao dele

--- src/db/config.js
+++ src/db/config.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
-module.exports = () =>
+module.exports = () => 
     open({
-        filename: '/src/db/rocketq.sqlite',
-        driver: sqlite3.Database,
+        filename: './src/db/rocketq.sqlite',
+        driver: sqlite3.Database
     })
 
 
