8f4a3c5d46d3aa3926729e54b213e02a4fed8fc7
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-25 00:02:37 -0300

    registra os dados no banco de dados

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,13 +1,30 @@
+const Database = require('../db/config');
+
+
 module.exports = {
-    create(req, res) {
+    async create(req, res) {
+
+        const db = await Database();
+        const password = req.body.password;
         let roomId;
 
         for (let i = 0; i < 6; i++) {
-
             (i == 0) ? 
                 roomId = Math.floor(Math.random() * 10).toString() : 
                 roomId += Math.floor(Math.random() * 10).toString();
         }
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${+roomId},
+            ${password}
+        )`);
+
+        await db.close();
+
+
         res.redirect(`/room/${roomId}`);
     }
 }
