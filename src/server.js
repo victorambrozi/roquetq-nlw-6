b244f6a9af8ae640900c14a74d7e677cd132a439
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-24 22:19:40 -0300

    comentarios para explicaçao de codigo

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ const server = express();
 
 server.set('view engine', 'ejs'); // inicia o ejs
 
-server.use(express.static("public"));
+server.use(express.static("public")); // indica o caminho da pasta public para o servidor renderizar 
 
-server.set('views', path.join(__dirname, 'views'));
+server.set('views', path.join(__dirname, 'views')); // renderiza os arquivos ejs
 
 server.use(express.urlencoded({extended: true})); // ----- middleware ----- decodifica o conteudo do formulário e passa para o .post()
 
 server.use(route); // utiliza as rotas importadas
 
-server.listen(3000, () => console.log('Rodando'));
+server.listen(3000, () => console.log('Rodando')); // indica a porta que o servidor deve rodar
