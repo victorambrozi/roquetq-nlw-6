35e9ae68cad8245f486fdfe2b9e4fe03ffe2b272
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-24 21:48:16 -0300

    adiciona logica na renderizaçao da pagina

--- src/route.js
+++ src/route.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 // .get() - captura algo
 // .post() - envia ago
-router.get('/', (req, res) => res.render("index"));
+router.get('/', (req, res) => res.render("index", {page: "enter-room"}));
+router.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}));
+
 router.get('/room', (req, res) => res.render("room"));
-router.get('/create-pass', (req, res) => res.render("create-pass"));
 
 // Formato que o formulario de dentro da modal tem que passar a informação
 router.post('/room/:room/:question/:action', questionController.index)  //res.param.variavel (room, question, action)
