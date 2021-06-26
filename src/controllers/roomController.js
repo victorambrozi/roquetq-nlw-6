14d9b7a380731d92d7ac51b27aeaa8612b8d8ac0
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-26 08:58:03 -0300

    verifica se o numero da sala ja existe

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,3 +1,4 @@
+const { open } = require('sqlite');
 const Database = require('../db/config');
 
 
@@ -7,24 +8,40 @@ module.exports = {
         const db = await Database();
         const password = req.body.password;
         let roomId;
+        let isRoom = true;
 
-        for (let i = 0; i < 6; i++) {
-            (i == 0) ? 
-                roomId = Math.floor(Math.random() * 10).toString() : 
-                roomId += Math.floor(Math.random() * 10).toString();
+        while (isRoom) {
+            // gera o numero da sala
+            for (let i = 0; i < 6; i++) {
+                (i == 0) ?
+                    roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString();
+            }
+
+            // verifica se o numero ja existe
+            const roomExistIds = await db.all(`SELECT id FROM rooms`);
+            isRoom = roomExistIds.some((roomExistId) => roomExistId === roomId);
+
+            if (!isRoom) {
+                // insere a sala no banco
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${+roomId},
+                    ${password}
+                )`);
+            }
         }
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${+roomId},
-            ${password}
-        )`);
 
         await db.close();
 
-
         res.redirect(`/room/${roomId}`);
+    },
+
+    open(req, res) {
+        const roomId = req.params.room;
+     res.render("room", {roomId: roomId})
     }
+
 }
--- src/route.js
+++ src/route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', (req, res) => res.render("index", {page: "enter-room"}));
 router.get('/create-pass', (req, res) => res.render("index", {page: "create-pass"}));
 
-router.get('/room/:room', (req, res) => res.render("room"));
+router.get('/room/:room', roomController.open);
 
 // Formato que o formulario de dentro da modal tem que passar a informação
 router.post('/question/:room/:question/:action', questionController.index)  //res.param.variavel (room, question, action)
