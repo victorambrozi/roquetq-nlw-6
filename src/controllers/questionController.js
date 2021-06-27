bd1eb8c6520f4a675189a4535e859d74066a97fa
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-27 10:39:12 -0300

    registra as questoes no banco de dados

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,15 +1,34 @@
+const Database = require("../db/config");
+
 module.exports = {
+    
     index(req, res) {
         const roomId = req.params.room;
         const questionId = req.params.question;
         const action = req.params.action;
         const passowrd = req.body.password;
-
+        
         console.log(roomId, questionId, action,passowrd)
     },
+    
+    async create(req, res) {
+        const db = await Database();
 
-    create(req, res) {
         const question = req.body.question;
-        const room = req.params.room;
+        const roomId = req.params.room;
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read
+        ) VALUES (
+            "${question}", 
+            ${roomId},
+            0
+        )`);
+
+        res.redirect(`/room/${roomId}`);
+
+        await db.close();
     }
 }
