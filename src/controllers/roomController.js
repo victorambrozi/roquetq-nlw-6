d9af9e839c297352baac20809d59fdb8e4071047
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-28 14:56:44 -0300

    cria rota para entrar na sala com o codigo

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -46,6 +46,12 @@ module.exports = {
 
         res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead });
 
+    },
+
+    enter(req, res) {
+        const roomId = req.body.roomId;
+
+        res.redirect(`/room/${roomId}`);
     }
 
 }
--- src/route.js
+++ src/route.js
@@ -12,7 +12,7 @@ router.get('/create-pass', (req, res) => res.render("index", {page: "create-pass
 // Formato que o formulario de dentro da modal tem que passar a informação
 router.post('/create-room', roomController.create);
 router.get('/room/:room', roomController.open);
-// router.post('/enterroom', roomController.enter);
+router.post('/enterroom', roomController.enter);
 
 router.post('/question/create/:room', questionController.create);
 router.post('/question/:room/:question/:action', questionController.index);
