6e9c9c51823026f993c22e7d02b0f518999f8639
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-28 15:52:04 -0300

    inclusao da tela sem perguntas

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -44,7 +44,15 @@ module.exports = {
         const questions = await db.all(`SELECT * FROM questions WHERE room  = ${roomId} and read = 0`);
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`);
 
-        res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead });
+        let isNoQuestions;
+
+        if (questions == 0) {
+            if (questionsRead.length == 0) {
+                isNoQuestions = true;
+            };
+        };
+
+        res.render("room", { roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions });
 
     },
 
